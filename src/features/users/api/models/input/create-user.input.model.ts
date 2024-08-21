04fe5baa19b8d3b027b53cc3aaa01ee75d81c04d
Your Name <you@example.com>
2024-08-21 15:53:37 +0300

    add custom decorator EmailIsExist

--- src/features/users/api/models/input/create-user.input.model.ts
+++ src/features/users/api/models/input/create-user.input.model.ts
@@ -1,6 +1,7 @@
 import { IsString, Length } from 'class-validator';
 import { Trim } from '../../../../../infrastructure/decorators/transform/trim';
 import { LoginIsExist } from '../../../../../infrastructure/decorators/validate/login-is-exist.decorator';
+import { EmailIsExist } from '../../../../../infrastructure/decorators/validate/email-is-exist.decorator';
 
 export class UserCreateModel {
   @IsString()
@@ -8,6 +9,27 @@ export class UserCreateModel {
   @Length(3, 10, { message: 'Length not correct' })
   @LoginIsExist()
   login: string;
+  @IsString()
+  @Trim()
+  @Length(6, 20, { message: 'Length not correct' })
   password: string;
+  @IsString()
+  @Trim()
+  @Length(3, 10, { message: 'Length not correct' })
+  @EmailIsExist()
   email: string;
 }
+
+const userEmailInputValidator = body('email')
+  .isString()
+  .withMessage('email is missing or not a string')
+  .trim()
+  .isLength({ min: 1 })
+  .withMessage('email is required')
+  .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+
+export const userInputValidator = [
+  userLoginInputValidator,
+  userPasswordInputValidator,
+  userEmailInputValidator,
+];
---
+++ src/infrastructure/decorators/validate/email-is-exist.decorator.ts
@@ -0,0 +1,48 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+import { Injectable } from '@nestjs/common';
+import { UsersRepository } from '../../../features/users/infrastructure/users.repository';
+import { ValidationArguments } from 'class-validator/types/validation/ValidationArguments';
+
+// Обязательна регистрация в ioc
+@ValidatorConstraint({ name: 'EmailIsExist', async: true })
+@Injectable()
+export class EmailIsExistConstraint implements ValidatorConstraintInterface {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  // validation logic
+  async validate(email: string, validationArguments?: ValidationArguments) {
+    const user = await this.usersRepository.findByField('email', email); // Checking if comment belongs to selected user
+
+    if (!user) {
+      return true;
+    }
+    return false;
+  }
+
+  // default error message
+  defaultMessage(validationArguments?: ValidationArguments): string {
+    return `Email ${validationArguments?.value} already exists`;
+  }
+}
+
+// decorator
+// https://github.com/typestack/class-validator?tab=readme-ov-file#custom-validation-decorators
+export function EmailIsExist(
+  property?: string,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [property],
+      validator: EmailIsExistConstraint,
+    });
+  };
+}
