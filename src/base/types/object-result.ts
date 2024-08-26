7cd031ab1b099be3c84c776edca4f61cf00b8963
Vladimir <vladimirleonov789@gmail.com>
2024-08-26 23:15:59 +0300

    moved object result pattern into classes

--- src/base/types/object-result.ts
+++ src/base/types/object-result.ts
@@ -8,25 +8,64 @@ export enum ResultStatus {
   InternalError = 'Internal Error',
 }
 
-// export class Result<T = null> {
+// export type Result<T = null> = {
 //   status: ResultStatus;
-//   errorMessage: string | null;
-//   extensions: Array<{ field: string; message: string }>;
+//   errorMessage?: string;
+//   extensions?: [{ field: string; message: string }];
 //   data: T;
-//
-//   constructor(
-//     status: ResultStatus,
-//     data: T = null,
-//     errorMessage?: string,
-//     extensions?: Array<{ field: string; message: string }>,
-//   ) {
-//
-//   }
-// }
+// };
 
-export type Result<T = null> = {
+export class Result<T = null> {
   status: ResultStatus;
-  errorMessage?: string;
-  extensions?: [{ field: string; message: string }];
   data: T;
-};
+  errorMessage?: string;
+  extensions?: Array<{ field: string; message: string }>;
+
+  constructor(
+    status: ResultStatus,
+    data: T,
+    errorMessage?: string,
+    extensions?: Array<{ field: string; message: string }>,
+  ) {
+    this.status = status;
+    this.data = data;
+    this.errorMessage = errorMessage;
+    this.extensions = extensions;
+  }
+  static success<T = null>(data: T | null = null): Result<T | null> {
+    return new Result(ResultStatus.Success, data);
+  }
+  static badRequest<T = null>(
+    extensions?: Array<{ field: string; message: string }>,
+    errorMessage?: string,
+  ): Result<T> {
+    return new Result(
+      ResultStatus.BadRequest,
+      null as any,
+      errorMessage,
+      extensions,
+    );
+  }
+  static notFound<T = null>(
+    extensions?: Array<{ field: string; message: string }>,
+    errorMessage?: string,
+  ): Result<T> {
+    return new Result(
+      ResultStatus.NotFound,
+      null as any,
+      errorMessage,
+      extensions,
+    );
+  }
+  static unauthorized<T = null>(
+    extensions?: Array<{ field: string; message: string }>,
+    errorMessage?: string,
+  ): Result<T> {
+    return new Result(
+      ResultStatus.Unauthorized,
+      null as any,
+      errorMessage,
+      extensions,
+    );
+  }
+}
