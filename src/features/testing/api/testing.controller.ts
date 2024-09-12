65d61ef178edb0e95d062b55232021a3eae844f4
Vladimir <vladimirleonov789@gmail.com>
2024-09-12 20:32:55 +0300

    refactor testing module

--- src/features/testing/api/testing.controller.ts
+++ src/features/testing/api/testing.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Delete, HttpCode } from '@nestjs/common';
+import { Controller, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { TestingService } from '../application/testing.service';
 
 @Controller('testing')
 export class TestingController {
   constructor(private readonly testingService: TestingService) {}
+
   @Delete('all-data')
-  @HttpCode(204)
+  @HttpCode(HttpStatus.NO_CONTENT)
   async deleteAllData() {
     return this.testingService.deleteAllData();
   }
